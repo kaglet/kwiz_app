1629c7cdfaa59d940c4c6558f110a479d2be5284
Kago Motlhabane <kmotlhabane769@gmail.com>
2023-04-11 13:54:52 +0200

    Fix white blank screen on home

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:kwiz_v2/pages/home.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
