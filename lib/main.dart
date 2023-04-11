e9e2dbeea0781427b01ccf033c779b10ac9244fc
Kago Motlhabane <kmotlhabane769@gmail.com>
2023-04-11 13:51:08 +0200

    Fix Home class import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:kwiz_v2/pages/home.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
