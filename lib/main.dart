a3e99a555bafe5e8aeecd819ab4745a5528808bf
Kago Motlhabane <kmotlhabane769@gmail.com>
2023-04-14 21:22:03 +0200

    Remove wrapper and uncomment sign out method

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ Future<void> main() async {
           titleLarge: TextStyle(color: Colors.white),
         ),
       ),
-      home: Wrapper(),
+      home: Home(),
     ),
   ));
 }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -57,14 +57,13 @@ class AuthService {
     }
   }
 
-//   //sign out
-//   Future signOut() async {
-//     try {
-//       return await _auth.signOut();
-//     } catch (e) {
-//       print(e.toString());
-//       return null;
-//     }
-//   }
-// }
+  //sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 }
