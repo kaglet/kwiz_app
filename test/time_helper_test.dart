6076446ca238f858d5648bb6744c155ec121cf83
Kago Motlhabane <kmotlhabane769@gmail.com>
2023-04-21 20:20:28 +0200

    Change test back to work

--- test/time_helper_test.dart
+++ test/time_helper_test.dart
@@ -13,15 +13,10 @@ import 'package:kwiz_v2/main.dart';
 void main() {
   // TODO 1: Write this
   // When we have a certain dateTime we want that it returns night
+  test("Expect one is one", () {
+    expect(1, 1);
+  });
   // if testing a class and its functions group tests by class then function
   // import class then ask for its function
   // Dependencies to unit test must be remvoed
-  test('One should be one', () {
-    // Arrange
-    int expectedNumber = 1;
-    // Act
-
-    // Assert
-    expect(expectedNumber, 2);
-  });
 }
