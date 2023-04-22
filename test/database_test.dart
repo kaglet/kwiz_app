986ee10faf3e0c062a180409271729eb2936a43f
Kago Motlhabane <kmotlhabane769@gmail.com>
2023-04-22 22:03:16 +0200

    Implement mock database

--- test/database_test.dart
+++ test/database_test.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
@@ -6,34 +7,13 @@ import 'package:kwiz_v2/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:kwiz_v2/services/database.dart';
-
-const CategoryCollection = 'categories';
-const UserCollection = 'users';
-const QuizCollection = 'quizzes';
+import 'package:kwiz_v2/services/mock_database.dart';
 
 void main() {
   test('get categories', () async {
-    // Populate the fake database.
-    final firestore = FakeFirebaseFirestore();
-    late List? categories = ['Art', 'Science'];
-    late List? testCategories = [];
-    await firestore
-        .collection(CategoryCollection)
-        .doc('gbdJOUgd8F5z26sKfjxu')
-        .set({
-      'CategoryName': categories,
-    });
-    // Get doc from Category collection
-    DocumentSnapshot docSnapshot = await firestore
-        .collection(CategoryCollection)
-        .doc('gbdJOUgd8F5z26sKfjxu')
-        .get();
+    final service = MockDataService();
+    final List? testCategories = await service.getCategories();
 
-    // Get data from doc and return as array
-    testCategories = docSnapshot['CategoryName'];
-
-    expect(testCategories, categories);
+    expect(testCategories, ['Art', 'Science']);
   });
-
-  test();
 }
