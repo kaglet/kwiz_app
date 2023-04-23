eabeaa696fdeb56adfb643c21edaddd758c7ff97
Aidan Brickhill <2427688@students.wits.co.za>
2023-04-23 20:16:54 +0200

    took out wrong quiz test

--- lib/services/mock_database.dart
+++ lib/services/mock_database.dart
@@ -18,28 +18,6 @@ const UserCollection = 'users';
 const QuizCollection = 'quizzes';
 
 class MockDataService extends Mock implements DatabaseService {
-  Future<void> _addQuestionDocument(
-      {String? quizID, Question? question}) async {
-    // QuizCollection.doc(quizID).collection('Questions').add({
-    //   'QuestionAnswer': question!.questionAnswer,
-    // // 'QuestionMark': Question!.QuestionMark,
-    // 'QuestionNumber': question.questionNumber,
-    // 'QuestionText': question.questionText,
-    // });
-    final firestore = FakeFirebaseFirestore();
-    await firestore
-        .collection(QuizCollection)
-        .doc(quizID)
-        .collection('Questions')
-        .doc()
-        .set({
-      'QuestionAnswer': question!.questionAnswer,
-      // 'QuestionMark': Question!.QuestionMark,
-      'QuestionNumber': question.questionNumber,
-      'QuestionText': question.questionText,
-    });
-  }
-
   @override
   Future<List?> getCategories() async {
     final firestore = FakeFirebaseFirestore();
@@ -61,68 +39,6 @@ class MockDataService extends Mock implements DatabaseService {
     testCategories = docSnapshot['CategoryName'];
 
     return (categories);
-  }
-
-  Future<Quiz?> addQuizWithQuestions(Quiz quizInstance) async {
-    late List<Question> questions = [];
-    final firestore = FakeFirebaseFirestore();
-    await firestore.collection(QuizCollection).doc(quizInstance.quizID).set({
-      'quizName': quizInstance.quizName,
-      'quizAuthor': quizInstance.quizName,
-      'quizCategory': quizInstance.quizName,
-      'quizDescription': quizInstance.quizName,
-      'quizMark': quizInstance.quizName,
-      'quizDateCreated': quizInstance.quizName,
-      'quizID': quizInstance.quizName,
-      'quizQuestions': questions,
-    });
-
-    quizInstance.quizQuestions.forEach((question) async {
-      await _addQuestionDocument(
-          quizID: quizInstance.quizID, question: question);
-    });
-
-    await firestore
-        .collection(QuizCollection)
-        .doc(quizInstance.quizID)
-        .collection('Questions')
-        .get();
-    DocumentSnapshot docSnapshot = await firestore
-        .collection(QuizCollection)
-        .doc(quizInstance.quizID)
-        .get();
-
-    Quiz quizOutput = Quiz(
-        quizName: docSnapshot['QuizName'],
-        quizCategory: docSnapshot['QuizCategory'],
-        quizDescription: docSnapshot['QuizDescription'],
-        quizMark: 0,
-        quizDateCreated: docSnapshot['QuizDateCreated'],
-        quizQuestions: questions,
-        quizID: docSnapshot.id,
-        quizAuthor: docSnapshot['QuizAuthor']);
-
-    await firestore
-        .collection(CategoryCollection)
-        .doc('gbdJOUgd8F5z26sKfjxu')
-        .get();
-
-    QuerySnapshot collectionSnapshot = await quizCollection
-        .doc(quizInstance.quizID)
-        .collection('Questions')
-        .get();
-    for (int i = 0; i < collectionSnapshot.docs.length; i++) {
-      var docSnapshot = collectionSnapshot.docs[i];
-      Question question = Question(
-          questionNumber: docSnapshot['QuestionNumber'],
-          questionText: docSnapshot['QuestionText'],
-          questionAnswer: docSnapshot['QuestionAnswer'],
-          questionMark: 0);
-
-      questions.add(question);
-    }
-
-    return quizOutput;
   }
 
   @override
