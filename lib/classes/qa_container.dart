fe3bfcd4efe0696c97bb6b728595287b703fd805
Aidan Brickhill <2427688@students.wits.co.za>
2023-05-05 15:06:46 +0200

    Added Fill in the blank option and changed QA extract class to allow for database functionality

--- lib/classes/qa_container.dart
+++ lib/classes/qa_container.dart
@@ -9,6 +9,8 @@ class QAContainer extends StatefulWidget {
 
   // create a question and answer controller to receive this widget's question and answer text field inputs
   final _questionController = TextEditingController();
+  final _questionPreController = TextEditingController();
+  final _questionPostController = TextEditingController();
   final _answerController = TextEditingController();
   int? number;
 
@@ -22,16 +24,36 @@ class QAContainer extends StatefulWidget {
     this.number = number ?? 0;
   }
 
+
   // for this  qaContainer which encapsulates data extract the question and answer data
   QA extractQA() {
     // return QA object with its question and answer text assigned from the respective controllers
+    if (qaType== 'shortAnswer'){
     return QA(
-        question: _questionController.text, answer: _answerController.text);
+        question: _questionController.text, answer: _answerController.text, type:'shortAnswer');
+    }
+    if (qaType== 'fillInTheBlank'){
+    return QA(
+        question: _questionPreController.text + "**"+ _questionPostController.text, answer: _answerController.text, type:'fillInTheBlank');
+    }
+     if (qaType== 'multipleChoice'){
+    return QAMultiple(
+        // answer option will come from all answerOptions controllers
+        question: _questionController.text, answer: _answerController.text, type:'multipleChoice', answerOptions: []);
+    }
+    else{
+      return QA(
+        question: _questionController.text, answer: _answerController.text,type:'shortAnswer');
+    }
+    }
+
+    @override
+    State<QAContainer> createState() => _QAContainerState();
   }
 
-  @override
-  State<QAContainer> createState() => _QAContainerState();
-}
+
+
+
 
 class _QAContainerState extends State<QAContainer> {
   @override
@@ -171,6 +193,191 @@ class _QAContainerState extends State<QAContainer> {
             const SizedBox(
               height: 20.0,
             )
+          ],
+        ),
+      );
+      return scrollview;
+    }
+
+    if (widget.qaType == 'fillInTheBlank') {
+      // return short answer qa container
+      SingleChildScrollView scrollview = SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    // invokes widget.delete method for this widget. It's like using this.delete and this.key except that changes for stateful widgets.
+                    // pass in the current widget's unique key to delete the current widget
+                    widget.delete(widget.key);
+                  },
+                  icon: const Icon(Icons.delete, color: Colors.white),
+                ),
+              ],
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color.fromARGB(255, 45, 64, 96),
+                    Color.fromARGB(255, 45, 64, 96),
+                  ],
+                ),
+              ),
+              child: SizedBox(
+                  height: 100,
+                  child: TextField(
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Nunito',
+                    ),
+                    // assign controller to this question textfield
+                    controller: widget._questionPreController,
+                    minLines: 3,
+                    maxLines: 3,
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration(
+                      alignLabelWithHint: true,
+                      labelText: 'Question ${widget.number} - pre Blank',
+                      labelStyle: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                      hintText: 'Question ${widget.number} - pre Blank',
+                      hintStyle: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5),
+                        ),
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                    ),
+                  )),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color.fromARGB(255, 45, 64, 96),
+                    Color.fromARGB(255, 45, 64, 96),
+                  ],
+                ),
+              ),
+              child: Row(
+                children: [
+                  Flexible(
+                    child: TextField(
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Nunito',
+                      ),
+                      // assign controller to this answer textfield
+                      controller: widget._answerController,
+                      minLines: 1,
+                      maxLines: 1,
+                      keyboardType: TextInputType.multiline,
+                      decoration: const InputDecoration(
+                        alignLabelWithHint: true,
+                        labelText: 'Blank Space',
+                        labelStyle: TextStyle(
+                          color: Colors.grey,
+                        ),
+                        hintText: 'Blank Space',
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(5),
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color.fromARGB(255, 45, 64, 96),
+                    Color.fromARGB(255, 45, 64, 96),
+                  ],
+                ),
+              ),
+              child: SizedBox(
+                  height: 100,
+                  child: TextField(
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Nunito',
+                    ),
+                    // assign controller to this question textfield
+                    controller: widget._questionPostController,
+                    minLines: 3,
+                    maxLines: 3,
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration(
+                      alignLabelWithHint: true,
+                      labelText: 'Question ${widget.number} - post Blank',
+                      labelStyle: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                      hintText: 'Question ${widget.number} - post Blank',
+                      hintStyle: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5),
+                        ),
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                    ),
+                  )),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
           ],
         ),
       );
