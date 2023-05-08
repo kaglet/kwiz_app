f482a4a28a99d1783e93125f33e68d91152669c8
Kago Motlhabane <kmotlhabane769@gmail.com>
2023-05-08 20:17:06 +0200

    Implement full multiple choice question type

--- lib/classes/qa_obj.dart
+++ lib/classes/qa_obj.dart
@@ -2,14 +2,17 @@ class QA {
   String question;
   String answer;
   String type;
-  
+
   QA({required this.question, required this.answer, required this.type});
 }
 
 class QAMultiple extends QA {
   List<String> answerOptions;
-  
-  QAMultiple({required  String question, required String answer, required String type, required this.answerOptions}) 
-  : super(question: question, answer: answer, type: type);
-}
 
+  QAMultiple(
+      {required String question,
+      required String answer,
+      required String type,
+      required this.answerOptions})
+      : super(question: question, answer: answer, type: type);
+}
